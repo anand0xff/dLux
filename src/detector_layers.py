1b4b63452f4b087bd0623d49ddcc6a9b0df63d54
Louis Desdoigts <ldes9555@uni.sydney.edu.au>
2022-07-17 03:06:11 +1000

    Commiting for merging main -> JWST dev but actually this time

--- src/detector_layers.py
+++ src/detector_layers.py
@@ -65,3 +65,71 @@ class ApplySaturation(eqx.Module):
         image_out = np.minimum(image, self.saturation)
         return image_out
     
+class AddConstant(eqx.Module):
+    """
+    Reduces any values above self.saturation to self.saturation
+    """
+    value: float
+    
+    def __init__(self, value):
+        self.value = np.array(value).astype(float)
+        
+    def __call__(self, image):
+        """
+        
+        """
+        # Apply saturation
+        image_out = image + self.value
+        return image_out
+    
+class IntegerDownsample(eqx.Module):
+    """
+    Downsamples an input image by an integer number of pixels via a sum.
+    The number of pixels in must by integer divisible by the kernel_size,
+    ie the following must be True: image.shape[0]%self.kernel_size == 0
+    
+    Parameters
+    ----------
+    kernel_size: int
+        The size of the downsampling kernel
+            
+    """
+    kernel_size: int
+    
+    def __init__(self, kernel_size):
+        self.kernel_size = int(kernel_size)
+        
+    def __call__(self, image):
+        """
+        
+        """
+        image_out = self.downsample(image, self.kernel_size)
+        return image_out
+    
+    def downsample(self, array, kernel_size):
+        """
+        Downsamples a kernel_size*n size array by kernel_size
+        
+        TODO: Properly test arbitrary dimensions
+        
+        """
+        size = array.shape[0]
+        size_out = size//kernel_size
+        
+        # # Iterate over all dimensions
+        # for i in range(len(array.shape)):
+        #     array = array.reshape([size*size_out, kernel_size]).sum(i)
+        #     array = array.reshape([size, size_out]).T
+
+        # Dim 1
+        # array = array.reshape([size**2 // kernel_size, kernel_size])
+        array = array.reshape([size*size_out, kernel_size]).sum(1)
+        array = array.reshape(size, size_out).T
+        # array = array.T
+
+        # Dim 2
+        array = array.reshape(size_out**2, kernel_size).sum(1)
+        # array = np.sum(array, axis=1)
+        array = array.reshape(size_out, size_out).T
+        # array = array.T
+        return array
